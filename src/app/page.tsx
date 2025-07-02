12d303b0032a90112ddca37faca36f7ae8af9c56
Toweave <toweave@163.com>
2025-07-02 14:47:39 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { Search, ChevronDown } from 'lucide-react';
 
 export default function Home() {
@@ -82,6 +83,10 @@ export default function Home() {
             <div className="mx-auto max-w-6xl"><h1
               className="text-2xl/8 font-semibold text-zinc-950 sm:text-xl/8 dark:text-white">Inbox</h1>
               <hr role="presentation" className="mt-6 w-full border-t border-zinc-950/10 dark:border-white/10" />
+            </div>
+
+            <div>
+              <Button>Click me</Button>
             </div>
           </div>
         </main>
